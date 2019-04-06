c6047688a294a0c63a0afa63b4fb0ebda91af8d1
guni1192 <ad2314ce71926@gmail.com>
2019-04-06 14:28:04 +0900

    fix go fmt

--- libcouml/util.go
+++ libcouml/util.go
@@ -8,7 +8,7 @@ import (
 func setEnv(env []string) {
 	for _, e := range env {
 		// PATH=/bin -> ["PATH", "/bin"]
-		envSet := strings.Split(e,"=")
+		envSet := strings.Split(e, "=")
 		if len(envSet) > 1 {
 			os.Setenv(envSet[0], envSet[1])
 		}
